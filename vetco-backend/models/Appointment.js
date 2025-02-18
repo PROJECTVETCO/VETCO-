40756b4efdf352a96a0e86e55cf44fb1aa08404f
ottojoash <louisjoshbricks@gmail.com>
2025-02-18 17:38:00 +0300

    app

--- vetco-backend/models/Appointment.js
+++ vetco-backend/models/Appointment.js
@@ -5,8 +5,8 @@ const AppointmentSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   time: { type: String, required: true },
   clientName: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // ✅ Associate with logged-in user
-  vetId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, // Optional vet assignment
+  // userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // ✅ Associate with logged-in user
+  // vetId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }, // Optional vet assignment
   status: { type: String, default: "Scheduled" },
   createdAt: { type: Date, default: Date.now },
 });
