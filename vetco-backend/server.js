395c97d5f5c31d77ce3d2514be23c5c2ca38da23
ottojoash <louisjoshbricks@gmail.com>
2025-02-18 17:28:45 +0300

    server

--- vetco-backend/server.js
+++ vetco-backend/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors({
   origin: "*", // Allow all origins (for development)
   methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
+  credentials: true,
+  allowedHeaders: ["Content-Type,Authorization"],
 }));
 
 app.use(express.json());
